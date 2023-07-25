cc72daac1267a8d875d4e27d385e4cb2671b0b9d
ELMHARMACHE NASSIM <nassim.nassim.raja@gmail.com>
2023-07-25 11:57:02 +0000

    git add

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
-import  Error  from "./Error";
-import  Home  from "./pages/home/Home";
-import  About  from "./pages/about/About";
-import  Header  from "./layouts/Header";
-import  Skills  from "./pages/skills/Skills";
-import  Contact  from "./pages/contact/Contact";
-import  Works  from "./pages/works/Works";
+import Error from "./Error";
+import Home from "./pages/home/Home";
+import About from "./pages/about/About";
+import Header from "./layouts/Header";
+import Skills from "./pages/skills/Skills";
+import Contact from "./pages/contact/Contact";
+import Works from "./pages/works/Works";
+import Resume from "./pages/resume/Resume";
+import Services from "./pages/services/Services";
 
 export const App = () => {
   return (
@@ -16,12 +18,16 @@ export const App = () => {
         <Route path="*" element={<Error />} />
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/resume" element={<Resume />} />
         <Route path="/skills" element={<Skills />} />
+        <Route path="/services" element={<Services />} />
         <Route path="/works" element={<Works />} />
         <Route path="/contact-me" element={<Contact />} />
       </Routes>
       <About />
+      <Resume />
       <Skills />
+      <Services />
       <Works />
       <Contact />
     </>
